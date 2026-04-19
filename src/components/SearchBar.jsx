faf36f4f0b8e3629b4c3d51164f3d0b1291df2ba
Ronald Garcia <ronaldgm@gmail.com>
2026-04-19 17:26:17 -0400

    Add inputs and buttons in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,20 @@
 function SearchBar() {
     return (
         <div>
-            Hello from SearchBar.jsx
+            <div>
+                <button>Best Match</button>
+                <button>Highest Rated</button>
+                <button>Most Reviewed</button>
+            </div>
+
+            <div>
+                <input type="text" placeholder="Search Businesses" />
+                <input type="text" placeholder="Where?" />
+            </div>
+            
+            <div>
+                <button>Let's Go</button>
+            </div>
         </div>
     );
 }
