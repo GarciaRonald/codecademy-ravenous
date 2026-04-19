740974d11dabb145ebda9e0f00a3c0b7afdc203e
Ronald Garcia <ronaldgm@gmail.com>
2026-04-19 16:23:10 -0400

    Create placeholder info in Business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,6 +1,24 @@
 function Business() {
     return (
-        <h1>Hello from Business compoenent, ok?</h1>
+        <div>
+            <div>
+                <img src="#" alt="Empty Image" />
+            </div>
+            <div>
+                <h2>Name</h2>
+            </div>
+            <div>
+                <div>
+                    <p>Address</p>
+                    <p>City State Zip</p>
+                </div>
+                <div>
+                    <p>Category</p>
+                    <p>Rating</p>
+                    <p>Rating Count</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
