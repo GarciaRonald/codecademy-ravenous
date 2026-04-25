2a149806afda061d8fcfaf8ca56f7b84af54d21c
Ronald Garcia <ronaldgm@gmail.com>
2026-04-25 17:30:07 -0400

    Delete old sampleBus and use business prop in Business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,42 +1,32 @@
 import '../styles/Business.css';
 
-const sampleBus = {
-    imgSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Bordertown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-};
-
-function Business() {
+function Business(props) {
+    const { imgSrc, name, address, city, state, zipCode, category, rating, reviewCount } = props.business;
+    
     return (
         <div className='business'>
             <div className='img-container'>
                 <img
-                    src={sampleBus.imgSrc}
-                    alt={sampleBus.name}
+                    src={imgSrc}
+                    alt={name}
                 />
             </div>
 
             <div className='business-title'>
-                <h2>{sampleBus.name}</h2>
+                <h2>{name}</h2>
             </div>
 
             <div className='business-info'>
                 <div className='info-left'>
-                    <p>{sampleBus.address}</p>
-                    <p>{sampleBus.city}</p>
-                    <p> {sampleBus.state} {sampleBus.zipCode}</p>
+                    <p>{address}</p>
+                    <p>{city}</p>
+                    <p>{state} {zipCode}</p>
                 </div>
 
                 <div className='info-right'>
-                    <p className='orange cat'>{sampleBus.category.toUpperCase()}</p>
-                    <p className='orange'>{`${sampleBus.rating} star${sampleBus.rating > 1 && 's'}`}</p>
-                    <p>{`${sampleBus.reviewCount} review${sampleBus.reviewCount === 1 ? '' : 's'}`}</p>
+                    <p className='orange cat'>{category.toUpperCase()}</p>
+                    <p className='orange'>{`${rating} star${rating > 1 && 's'}`}</p>
+                    <p>{`${reviewCount} review${reviewCount === 1 ? '' : 's'}`}</p>
                 </div>
             </div>
         </div>
